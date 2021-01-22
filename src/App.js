69d6eed1e1ba36c112b1fe1a8302f8679c99712c
Shraddha Virupaxi <shraddha684@gmail.com>
2021-01-22 21:15:52 +0530

    Adding code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+let students = [{
+    label: "Name",
+    key: 'name'
+  },
+  {
+    label : "USN Number",
+    key : 'usn'
+  },
+  {
+    label : "Branch Name",
+    key : 'branch'
+  },
+  {
+    label : "Email",
+    key : 'email'
+  }
+
+]
+
+
+
 function App() {
+    const[studentData,setstudentData]=useState("");
+    
+
+      const handlerChange =(event) =>{
+        setstudentData(event.target.value);
+      };
+
+      const onSubmit=()=>{
+        setstudentData(studentData);
+
+      }
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      <form>
+      <fieldset>
+        Student Information
+        {
+          students.map(student =>{
+          return <div>
+            {student.label}
+          
+           <input value={studentData[student.key]}
+           onChange={handlerChange}/>
+          </div>
+        })}
+        <button onClick={onSubmit}>submit</button>
+      </fieldset>
+      </form>
+
     </div>
   );
 }
